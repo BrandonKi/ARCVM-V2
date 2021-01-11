d8249680fb0d8fd403e12b83022deabf24ffa69a
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-01-10 22:09:43 -0500

    started implementing the main loop

--- src/Arcvm.cpp
+++ src/Arcvm.cpp
@@ -14,8 +14,68 @@ void Arcvm::loadProgram(const u8 *program, size_t size) {
 }
 
 i32 Arcvm::run() {
-    // fetch
-    // decode
-    // execute
+
+    while(true) {
+        ++program_counter;
+        execute();
+    }
+
     return 0;
+}
+
+void Arcvm::execute() {
+    switch (program[program_counter]) {
+        case instruction::ret:
+            break;
+        case instruction::mov_register_value:
+
+            break;
+        case instruction::mov_register_address:
+            break;
+        case instruction::mov_register_register:
+            
+            break;
+        case instruction::mov_address_value:
+            break;
+        case instruction::mov_address_address:
+            break;
+        case instruction::mov_address_register:
+            break;
+        case instruction::push_value:
+            break;
+        case instruction::push_address:
+            break;
+        case instruction::push_register:
+            break;
+        case instruction::pop_register:
+            break;
+        case instruction::pop_address: 
+            break;
+        case instruction::add:
+            break;
+        case instruction::add_register_register:
+            break;
+        case instruction::sub:
+            break;
+        case instruction::sub_register_register:
+            break;
+        case instruction::mul:
+            break;
+        case instruction::mul_register_register:
+            break;
+        case instruction::div:
+            break;
+        case instruction::div_register_register:
+            break;
+        case instruction::mod:
+            break;
+        case instruction::mod_register_register:
+            break;
+        case instruction::jump:
+            break;
+        case instruction::call:
+            break;
+        default:
+            break;
+    }
 }
