c6282b27e4e0ea31d17f1118032e399e9b01950b
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-01-13 19:22:57 -0500

    wrote a test

--- tests/test0.cpp
+++ tests/test0.cpp
@@ -1,4 +1,35 @@
+#include "ARCVM.h"
 
+/**
+ * push two unsigned 64 bit values to the stack and add them
+ */
+#define vm Arcvm::instruction
 int main(int, char**) {
-    return 0;
+    
+    char program[] = {
+        vm::push_value,
+        0x64,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        vm::push_value,
+        0x64,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        0x00,
+        vm::addu,
+        vm::ret
+    };
+
+    Arcvm VM;
+    VM.loadProgram(program, sizeof(program));
+    return VM.run() == 0 ? 0 : 1;
 }
