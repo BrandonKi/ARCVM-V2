0d1ea3748a92fe0897c6fb62ff9f8ebdaacebd83
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-01-15 01:28:49 -0500

    added another test

---
+++ tests/test1.cpp
@@ -0,0 +1,32 @@
+#include "test.h"
+
+/**
+ * push two signed 64 bit values to the stack and add them
+ */
+int main() {
+
+    std::vector<u8> program = {
+        vm::push_value,
+        0xf0,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        vm::push_value,
+        0xf0,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        0xff,
+        vm::adds,
+        vm::ret
+    };
+    expect(-32);
+    return runTest(program);
+}
