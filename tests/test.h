a4d4906499e22095394b12d3176ce80fa9254593
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2021-01-15 01:28:28 -0500

    added runTest overload for unsigned chars

--- tests/test.h
+++ tests/test.h
@@ -6,12 +6,26 @@
 
 static i32 expected_result;
 
-void expect(i32 result) {
-    expected_result = result;
+/**
+ * the vm returns i32 so the only way to return meaningful values of other types 
+ * is to reinterpret them as an i32 then return
+ * 
+ * this function does the reinterpreting 
+ * so checking if something is an expected result of a different type is easier
+ */
+template <typename T>
+void expect(T result) {
+    expected_result = *reinterpret_cast<i32*>(&result);
 }
 
 i32 runTest(std::vector<char> program) {
     Arcvm VM;
     VM.loadProgram(program.data(), program.size());
+    return VM.run() == expected_result ? 0 : 1;
+}
+
+i32 runTest(std::vector<u8> program) {
+    Arcvm VM;
+    VM.loadProgram(reinterpret_cast<char*>(program.data()), program.size());
     return VM.run() == expected_result ? 0 : 1;
 }
